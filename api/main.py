2b687e81dfbe431f87881db19fb14c085a76a2bf
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-15 22:51:41 -0300

    feature: api starter

---
+++ api/main.py
@@ -0,0 +1,47 @@
+import uvicorn
+import logging
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from config.env_vars import get_allowed_hosts, get_port, get_enviroment
+from config.logging import setup_logger
+
+setup_logger()
+
+
+logger = logging.getLogger(__name__)
+
+allowed_hosts = get_allowed_hosts()
+
+app = FastAPI(
+    title="dt-labs IoT",
+    description="API to manage and monitoring IoT devices",
+    version="1.0.0",
+)
+
+CORSMiddleware(app, allow_origins=allowed_hosts)
+
+
+@app.get("/")
+def default():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    port = get_port()
+    env = get_enviroment()
+
+    logging.basicConfig(
+        level=logging.DEBUG if env == "development" else logging.INFO,
+        filename="api.log",
+    )
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        workers=1,
+        reload=True if env == "development" else False,
+    )
