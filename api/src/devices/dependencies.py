81635d8929327a460752d3f87f919067fc6f1828
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-18 15:34:21 -0300

    feature: devices endpoints

--- api/src/devices/dependencies.py
+++ api/src/devices/dependencies.py
@@ -9,4 +9,30 @@ def get_dependencie_collection(db: DatabaseDep) -> Collection:
     return devices_collection
 
 
+def get_devices_query_params(
+    location: str = "",
+    uuid: str = "",
+    sn: str = "",
+    skip: int = 0,
+    limit: int = 100,
+    created_at: str = "",
+):
+    q = dict()
+    if location:
+        q.update({"location": location})
+
+    if uuid:
+        q.update({"uuid": uuid})
+
+    if sn:
+        q.update({"sn": sn})
+
+    if created_at:
+        q.update({"creatd_at": created_at})
+
+    return {"q": q, "skip": skip, "limit": limit}
+
+
 DevicesCollectionDep = Annotated[Collection, Depends(get_dependencie_collection)]
+
+DevicesQueryParamsDep = Annotated[dict, Depends(get_devices_query_params)]
--- api/src/devices/router.py
+++ api/src/devices/router.py
@@ -1,22 +1,24 @@
 import logging
 from typing import Optional
+
+from fastapi import APIRouter, HTTPException, Request, status
 from pymongo.errors import ServerSelectionTimeoutError
 
-from fastapi import APIRouter, status, HTTPException
-from src.schemas import CommonsDep
-from .dependencies import DevicesCollectionDep
-
-from .schemas import Device, DeviceOut, DeviceCreated, DeviceForm
+from .dependencies import DevicesCollectionDep, DevicesQueryParamsDep
+from .schemas import Device, DeviceCreated, DeviceForm, DeviceOut, DeviceUpdateForm
 
 router = APIRouter(prefix="/devices", tags=["devices"])
 logger = logging.getLogger(__name__)
 
 
 @router.get("/", response_model=Optional[list[DeviceOut]])
-async def get_devices(commons: CommonsDep, collection: DevicesCollectionDep):
+async def get_devices(commons: DevicesQueryParamsDep, collection: DevicesCollectionDep):
+    logger.info(f"query_param: {commons['q']}")
     try:
-        devices = collection.find()
-        logger.debug("Retrieved devices: %s", devices)
+        devices = (
+            collection.find(commons["q"]).skip(commons["skip"]).limit(commons["limit"])
+        )
+        logger.debug(f"Retrieved devices: {devices}")
     except ServerSelectionTimeoutError as db_err:
         logger.error(f"Database connection error: {db_err}")
         raise HTTPException(status_code=503, detail="Database connection error")
@@ -28,31 +30,59 @@ async def get_devices(commons: CommonsDep, collection: DevicesCollectionDep):
 
 
 @router.get("/{device_id}", response_model=Device)
-async def get_device(device_id: str):
-    return
+async def get_device(device_id: str, collection: DevicesCollectionDep):
+    return collection.find_one({"uuid": device_id})
 
 
 @router.get("/{device_id}/status", response_model=Device)
-async def get_device_status(device_id: str, commons: CommonsDep):
+async def get_device_status(device_id: str, collection: DevicesCollectionDep):
     return
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=DeviceCreated)
-async def create_device(device: DeviceForm, collection: DevicesCollectionDep):
+async def create_device(
+    device: DeviceForm, request: Request, collection: DevicesCollectionDep
+):
     try:
-        collection.insert_one(device.model_dump())
+        new_device = device.model_dump()
+        new_device["user_id"] = request.state.user_id
+
+        collection.insert_one(new_device)
     except Exception as e:
         logger.error(f"Error creating device: {e}")
         raise HTTPException(status_code=500, detail="Unable to create device") from e
 
-    return device.model_dump()
+    return new_device
 
 
-@router.delete("/{device_id}")
-async def delete_device(device_id: str):
+@router.delete("/{device_id}", status_code=status.HTTP_200_OK)
+async def delete_device(
+    device_id: str, collection: DevicesCollectionDep, request: Request
+):
+    user_id = request.state.user_id
+    try:
+        collection.delete_one({"uuid": device_id, "user_id": user_id})
+    except Exception as e:
+        logger.error(f"Error deleting device: {device_id}\n detail: {e}")
+        raise HTTPException(status_code=400, detail="Unable to delete device") from e
     return
 
 
-@router.put("/{device_id}")
-async def update_device(device_id: str, device: Device):
+@router.put("/{device_id}", status_code=status.HTTP_200_OK)
+async def update_device(
+    device_id: str,
+    device: DeviceUpdateForm,
+    collection: DevicesCollectionDep,
+    request: Request,
+):
+    user_id = request.state.user_id
+    update_values = device.model_dump(exclude_unset=True)
+    filter = {"user_id": user_id, "uuid": device_id}
+
+    try:
+        collection.update_one(filter, {"$set": update_values})
+    except Exception as e:
+        logger.error(f"Error updating device: {device_id}\n detail: {e}")
+        raise HTTPException(status_code=400, detail="Unable to update device") from e
+
     return
