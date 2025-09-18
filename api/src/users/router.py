5f083553f4ed31ad48e9ab636c4a8af1c3e7ab1b
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-17 21:59:49 -0300

    feature: users end points

--- api/src/users/router.py
+++ api/src/users/router.py
@@ -1,15 +1,90 @@
-from fastapi import APIRouter, status
+import logging
+from uuid import uuid4
+from fastapi import APIRouter, Response, status, HTTPException
 
-from .schemas import UserIn, UserOut
+from src.schemas import DatabaseDep
 
-router = APIRouter(prefix="/devices", tags=["devices"])
+from .schemas import UserCreated, UserOut, UserInputForm, UserLoginForm
+from .utils import Hasher
+
+router = APIRouter(prefix="/users", tags=["devices"])
+logger = logging.getLogger(__name__)
 
 
-@router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-def create_user(user: UserIn):
-    return {}
+@router.post("/", response_model=UserCreated, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserInputForm, db: DatabaseDep):
+    users_collection = db.get_collection("users")
+
+    newUser = user.model_dump()
+    hashed_password = Hasher.get_password_hash(user.password)
+    newUser["password"] = hashed_password
+
+    try:
+        users_collection.insert_one(newUser)
+    except Exception as e:
+        logger.info(f"Failed to insert new user: {e}")
+        raise HTTPException(status_code=400, detail="Unable to create user")
+
+    return user
 
 
-@router.post("/login")
-def create_session():
+@router.get("/{user_id}", response_model=UserOut)
+def get_user(user_id: str, db: DatabaseDep):
+    users_collection = db.get_collection("users")
+    try:
+        logger.info(f"Fetching user with id: {user_id}")
+        user = users_collection.find_one({"id": user_id})
+    except Exception as e:
+        logger.info(f"Failed to insert new user: {e}")
+        raise HTTPException(status_code=400, detail="Unable to create user")
+
+    return user
+
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+def create_session(
+    user_data: UserLoginForm,
+    db: DatabaseDep,
+    response: Response,
+):
+    users_collection = db.get_collection("users")
+    session_collection = db.get_collection("sessions")
+
+    try:
+        user = users_collection.find_one({"username": user_data.username})
+
+        if not user:
+            raise ValueError("Invalid credentials")
+
+        logger.info(f"To be compared: {user_data.password} with {user['password']}")
+        password_valid = Hasher.verify_password(user_data.password, user["password"])
+        logger.info(f"Password valid: {password_valid}")
+
+        if not password_valid:
+            raise ValueError("Invalid credentials")
+
+        session_id = uuid4().hex
+
+        session_collection.insert_one(
+            {"user_id": user["_id"], "session_id": session_id}
+        )
+        session_collection.find_one_and_delete({"user_id": user["_id"]})
+
+        response.set_cookie(
+            key="session_id",
+            value=session_id,
+            httponly=True,
+            secure=False,
+            samesite="none",
+            expires=3600,
+            max_age=3600,
+        )
+
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
+
+    except Exception as e:
+        logger.info(f"Failed to create session: {e}")
+        raise HTTPException(status_code=400, detail="Unable to create session")
+
     return
--- api/src/users/schemas.py
+++ api/src/users/schemas.py
@@ -1,14 +1,48 @@
-from datetime import date
-from pydantic import BaseModel
+from uuid import uuid4
+import re
+
+from typing import Annotated
+from pydantic import BaseModel, Field, AfterValidator
+from fastapi import Form
+
+
+def check_password_pattern(password: str) -> str:
+    regex = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+    result = re.search(regex, password)
+
+    if result is None:
+        raise ValueError(
+            "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number."
+        )
+
+    return password
+
+
+SecurePassword = Annotated[str, AfterValidator(check_password_pattern)]
 
 
 class UserIn(BaseModel):
-    username: str
-    password: str
-    created_at: date
-    updated_at: date
+    id: str = Field(default_factory=lambda: uuid4().hex)
+    username: str = Field()
+    password: SecurePassword = Field()
+    model_config = {"extra": "forbid"}
+
+
+class UserLogin(BaseModel):
+    username: str = Field()
+    password: str = Field()
+    model_config = {"extra": "forbid"}
+
+
+UserInputForm = Annotated[UserIn, Form()]
+
+UserLoginForm = Annotated[UserLogin, Form()]
 
 
 class UserOut(BaseModel):
-    id: str
-    username: str
+    id: str = Field()
+    username: str = Field()
+
+
+class UserCreated(BaseModel):
+    id: str = Field()
