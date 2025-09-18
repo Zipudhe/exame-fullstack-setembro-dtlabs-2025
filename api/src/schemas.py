fce245ea8045ccd2926f481477b6340ff1de9924
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-18 15:33:02 -0300

    feature: update base database and config

--- api/src/schemas.py
+++ api/src/schemas.py
@@ -1,14 +1,22 @@
+import logging
 from fastapi import Depends
 from typing import Annotated
 
-from .database import get_database
+from .database import get_database, get_redis_storage
 from pymongo.database import Database
+from redis import Redis
+
+logger = logging.getLogger(__name__)
 
 
-async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+async def common_parameters(
+    q: str | None = None, location: str = "", skip: int = 0, limit: int = 100
+):
     return {"q": q, "skip": skip, "limit": limit}
 
 
 CommonsDep = Annotated[dict, Depends(common_parameters)]
 
 DatabaseDep = Annotated[Database, Depends(get_database)]
+
+RedisDep = Annotated[Redis, Depends(get_redis_storage)]
