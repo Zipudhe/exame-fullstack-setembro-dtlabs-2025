e1ed00dd1f3ec9fc2bede53ebf55e1cb162844cf
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-17 21:59:01 -0300

    feature: connection test on start up

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
+from contextlib import asynccontextmanager
 import uvicorn
 import logging
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
 from config.env_vars import get_allowed_hosts
@@ -9,6 +11,7 @@ from config.logging import setup_logger
 
 from src.devices.router import router as devicesRouter
 from src.users.router import router as usersRouter
+from src.database import check_client_connection
 
 setup_logger()
 
@@ -17,16 +20,25 @@ logger = logging.getLogger(__name__)
 
 allowed_hosts = get_allowed_hosts()
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("application custom startup")
+    check_client_connection()
+    yield
+
+
 app = FastAPI(
     title="dt-labs IoT",
     description="API to manage and monitoring IoT devices",
     version="1.0.0",
+    lifespan=lifespan,
 )
 
 CORSMiddleware(app, allow_origins=allowed_hosts)
 
 app.include_router(usersRouter, prefix="/api", tags=["Users"])
-app.include_router(devicesRouter, prefix="/api", tags=["Users"])
+app.include_router(devicesRouter, prefix="/api", tags=["Devices"])
 
 
 @app.get("/")
