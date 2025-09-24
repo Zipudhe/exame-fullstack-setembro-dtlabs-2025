4b22ccc80a91a265d55eab75c38e08522034578d
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-24 00:03:07 -0300

    feature: login

---
+++ frontend/app/lib/api.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: "http://localhost:8000/api",
+  withCredentials: true
+})
+
+export type LoginData = {
+  email: string,
+  password: string
+}
+
+export const login = (payload: FormData) => {
+  return api.post('/users/login', payload, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    withCredentials: true,
+  })
+}
+
