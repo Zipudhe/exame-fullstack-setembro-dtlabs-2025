92c5995744f5dcd1b2f5288e86d21aedb54cb605
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-26 13:44:20 -0300

    feature: devices endpoints methods

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
+import type { DetailedDevice, UpdateDevice } from 'types/device'
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api",
@@ -20,6 +21,41 @@ export const login = (payload: FormData) => {
 }
 
 export const logOut = () => {
-  return api.post('/users/logout', { withCredentias: true })
+  return api.delete('/users/logout', {
+    withCredentials: true,
+  })
 }
 
+
+export const getDevices = () => {
+  return api.get("/devices")
+}
+
+export const getDevice = (device_id: string): Promise<AxiosResponse<DetailedDevice>> => {
+  return api.get(`/devices/${device_id}`, {
+    withCredentials: true,
+  })
+}
+
+export const getDeviceStatus = (device_id: string) => {
+  return api.get(`/devices/${device_id}/status`)
+}
+
+export const updateDevice = (device_id: string, payload: UpdateDevice) => {
+  return api.put(`/devices/${device_id}`, payload, {
+    withCredentials: true,
+  })
+}
+
+export const deleteDevice = (device_id: string) => {
+  return api.delete(`/devices/${device_id}`)
+}
+
+export const createDevice = (payload: FormData) => {
+  return api.post('/devices', payload, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    withCredentials: true,
+  })
+}
