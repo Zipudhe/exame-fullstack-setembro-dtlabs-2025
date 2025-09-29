a47ad4c4710614e17ddd5fa8be3a6a4c6b224eeb
lucas.ramalho <ramalhodev.lucas@gmail.com>
2025-09-29 00:34:51 -0300

    feature: request ajustments

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,10 +1,14 @@
-import axios, { type AxiosResponse } from 'axios';
+import axios from 'axios';
 import type { DetailedDevice, UpdateDevice } from 'types/device'
+import type { NotificationConfig, NotificationConfigPayload, threshHoldConfig } from 'types/notification'
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api",
   withCredentials: true
 })
+
+const deviceBaseUrl = "/devices"
+const notificationsBaseUrl = "/notifications"
 
 export type LoginData = {
   email: string,
@@ -15,47 +19,63 @@ export const login = (payload: FormData) => {
   return api.post('/users/login', payload, {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
-    },
-    withCredentials: true,
+    }
   })
 }
 
 export const logOut = () => {
-  return api.delete('/users/logout', {
-    withCredentials: true,
-  })
+  return api.delete('/users/logout')
 }
 
 
 export const getDevices = () => {
-  return api.get("/devices")
+  return api.get(deviceBaseUrl)
 }
 
-export const getDevice = (device_id: string): Promise<AxiosResponse<DetailedDevice>> => {
-  return api.get(`/devices/${device_id}`, {
-    withCredentials: true,
-  })
+export const getDevice = (device_id: string) => {
+  return api.get<DetailedDevice>(`${deviceBaseUrl}/${device_id}`)
 }
 
 export const getDeviceStatus = (device_id: string) => {
-  return api.get(`/devices/${device_id}/status`)
+  return api.get(`${deviceBaseUrl}/${device_id}/status`)
 }
 
 export const updateDevice = (device_id: string, payload: UpdateDevice) => {
-  return api.put(`/devices/${device_id}`, payload, {
-    withCredentials: true,
-  })
+  return api.put(`/${deviceBaseUrl}/${device_id}`, payload)
 }
 
 export const deleteDevice = (device_id: string) => {
-  return api.delete(`/devices/${device_id}`)
+  return api.delete(`${deviceBaseUrl}/${device_id}`)
 }
 
 export const createDevice = (payload: FormData) => {
-  return api.post('/devices', payload, {
+  return api.post(deviceBaseUrl, payload, {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
-    },
-    withCredentials: true,
+    }
   })
+}
+
+export const getNotificationsConfig = () => {
+  return api.get<NotificationConfig[]>(`${notificationsBaseUrl}/config`)
+}
+
+export const createNotificationsConfig = (payload: threshHoldConfig) => {
+  return api.post(`${notificationsBaseUrl}/config`, payload, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+export const updateNotificationsConfig = (notificationId: string, payload: NotificationConfigPayload) => {
+  return api.put(`${notificationsBaseUrl}/config/${notificationId}`, payload, { headers: { 'Content-Type': 'application/json' } })
+}
+
+export const deleteNotificationsConfig = (notificationId: string) => {
+  return api.delete(`${notificationsBaseUrl}/config/${notificationId}`)
+}
+
+export const getNotificationConfig = (notificationId: string) => {
+  return api.get<NotificationConfig>(`${notificationsBaseUrl}/config/${notificationId}`)
 }
